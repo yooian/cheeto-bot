fb9edb5393d0dac2ab39e1ba98e2a8c8861c70de
yooian <yooian03@gmail.com>
2024-11-21 14:57:42 -0800

    Create Dockerfile and docker-compose

--- src/cheeto_bot.py
+++ src/cheeto_bot.py
@@ -14,13 +14,15 @@ DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_HOST = os.getenv('DB_HOST')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_PORT = os.getenv('DB_PORT')
 
 # Set up database connection
 connection = psycopg2.connect(
     dbname=DB_NAME,
     user=DB_USER,
     host=DB_HOST,
-    password=DB_PASSWORD
+    password=DB_PASSWORD,
+    port=DB_PORT
 )
 
 # Description
@@ -39,12 +41,16 @@ async def on_ready():
 
 @bot.command()
 async def cheeto(ctx):
-  with connection.cursor() as cursor:
-    cursor.execute("SELECT url FROM cheetos ORDER BY RANDOM() LIMIT 1;")
-    result = cursor.fetchone()
-    if result:
-        await ctx.send(result[0])
-    else:
-        await ctx.send("No Cheetos found :(")
-  
-bot.run(TOKEN)
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT url FROM cheetos ORDER BY RANDOM() LIMIT 1;")
+            result = cursor.fetchone()
+            if result:
+                await ctx.send(result[0])
+            else:
+                await ctx.send("No Cheetos found :(")
+    except psycopg2.Error as e:
+        # If an error occurs, roll back the transaction
+        connection.rollback()
+        await ctx.send("An error occurred while retrieving the image.")
+        print(f"Error: {e}")
